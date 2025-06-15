dec7989ecdf085add73fba5953d735ec8910d4b6
RLProteus <richylombano@gmail.com>
2025-06-15 18:28:43 -0400

    static test 3

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  assetPrefix: 'https://rlproteus.github.io/resume-nextjs/',
+  basePath: '/resume-nextjs',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
