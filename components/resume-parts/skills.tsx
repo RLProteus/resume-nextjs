e57ff835da9180367ad4e21964c5f1ab3b123cff
RLProteus <richylombano@gmail.com>
2025-06-15 22:44:11 -0400

    new skills style

--- components/resume-parts/skills.tsx
+++ components/resume-parts/skills.tsx
@@ -1,31 +1,61 @@
-import { Badge } from "@/components/ui/badge"
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { Progress } from "@/components/ui/progress"
+import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/components/ui/card"
 import { promises as fs } from 'fs';
+import { ProgressIndicator } from "@radix-ui/react-progress";
 
 
 type Skill = {
   name: string;
   level: string;
-  keywords: string[];
+  keywords: [{
+    name: string;
+    proficiency: string;
+    value: number;
+  }
+  ];
 };
-      
-export async function Skills()  {
-    const  file = await fs.readFile(process.cwd() + '/app/data/resume.json', 'utf8');
-    const resume = JSON.parse(file);
+
+type Keyword = {
+  name: string;
+  proficiency: string;
+  value: number;
+};
+
+export async function Skills() {
+  const file = await fs.readFile(process.cwd() + '/app/data/resume.json', 'utf8');
+  const resume = JSON.parse(file);
   return (
-      <Card>
-        <CardHeader className="pb-2">
-          <CardTitle>Skills</CardTitle>
-        </CardHeader>
-        <CardContent>
-         
-          <div className="flex flex-wrap gap-2">
-            {resume.skills.map((skill: Skill, index: number) => (
-            <Badge className="dark:bg-green-300" key={index}>{skill.name}</Badge>
-             ))}
-          </div>
-        </CardContent>
-      </Card>
-    )
+    <Card className="w-full">
+      <CardHeader>
+        <CardTitle>Skills</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          
+          {resume.skills.map((skill: Skill, index: number) => (
+            
+            <div key={index} className="space-y-3">
+              
+              <h3 className=" font-bold text-lg underline">{skill.name}</h3>
+              <div className="space-y-4">
+                {skill.keywords.map((keyword: Keyword, index: number) => (
+                  <div key={index} className="space-y-1">
+                    <div className="flex justify-between items-center">
+                      <span className="text-sm">{keyword.name}</span>
+                      <span className="text-sm text-muted-foreground">{keyword.value}%</span>
+                    </div>
+                    <Progress value={keyword.value} className="[&>*]:bg-slate-400 dark:bg-slate-800 bg-slate-200">
+                    </Progress>
+                  </div>
+                  
+                ))}
+              </div>
+
+            </div>
+          ))}
+        </div>
+      </CardContent>
+    </Card>
+  )
 }
 
