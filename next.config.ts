069f4d8381b18fce9678db57498e7736935fb899
RLProteus <richylombano@gmail.com>
2025-06-16 22:19:08 -0400

    use default base path

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  basePath: '/resume-nextjs',
   images: {
     unoptimized: true,
   },
