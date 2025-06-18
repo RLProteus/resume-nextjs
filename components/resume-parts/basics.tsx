f37770ea3b42cd2a44f0dab20c03e74cb8349a0b
RLProteus <richylombano@gmail.com>
2025-06-17 22:44:18 -0400

    icon updates

--- components/resume-parts/basics.tsx
+++ components/resume-parts/basics.tsx
@@ -1,5 +1,5 @@
 
-import { Mail, Phone, MapPin, Globe, Github, Linkedin } from "lucide-react"
+import { IconMail, IconMapPin, IconWorld, IconBrandGithub, IconBrandLinkedin } from "@tabler/icons-react"
 
 export function Basics(props: any) {
       let resume = props.resume;
@@ -8,18 +8,26 @@ export function Basics(props: any) {
         <h1 className="text-3xl font-bold">{resume.basics.name}</h1>
         <p className="text-xl text-muted-foreground">{resume.basics.label}</p>
         <div className="flex flex-wrap gap-3 pt-2">
-          <div className="flex items-center text-sm text-muted-foreground">
-            <Mail className="mr-1 h-4 w-4" />
-            {resume.basics.email}
+          <div className="flex items-center text-sm text-muted-foreground hover:text-blue-500">
+            <IconMail className="mr-1 h-4 w-4 " />
+            <a href={`mailto:${resume.basics.email}`}> {resume.basics.email} </a>
           </div>
     
           <div className="flex items-center text-sm text-muted-foreground">
-            <MapPin className="mr-1 h-4 w-4" />
+            <IconMapPin className="mr-1 h-4 w-4 " />
             {resume.basics.location.city}, {resume.basics.location.region} {resume.basics.location.countryCode}
           </div>
-          <div className="flex items-center text-sm text-muted-foreground">
-            <Globe className="mr-1 h-4 w-4" />
-            <a href={resume.basics.website}> Website</a>
+          <div className="flex items-center text-sm text-muted-foreground hover:text-blue-500">
+            <IconWorld className="mr-1 h-4 w-4 " />
+            <a className="" href={resume.basics.website}>Website</a>
+          </div>
+                    <div className="flex items-center text-sm text-muted-foreground hover:text-blue-500">
+            <IconBrandGithub className="mr-1 h-4 w-4" />
+            <a href={resume.basics.profiles[1].url}>Github</a>
+          </div>
+                    <div className="flex items-center text-sm text-muted-foreground hover:text-blue-500">
+            <IconBrandLinkedin className="mr-1 h-4 w-4" />
+            <a href={resume.basics.profiles[0].url}>LinkedIn</a>
           </div>
         </div>
       </div>
